dc5fb28fc32db4cd9ccb6cd761568a210c227e7e
khushiakter10 <khushiaktar2023@gmail.com>
2024-12-02 00:13:59 +0600

    meetup.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:online_learing_design/view/continue_with_phne/continue_with_phne.dart';
 import 'package:online_learing_design/view/create_your_own/create_your_own.dart';
 import 'package:online_learing_design/view/log_in/log_in.dart';
+import 'package:online_learing_design/view/meetup/meetup.dart';
 import 'package:online_learing_design/view/quick_and_easy/quick_and_easy.dart';
 import 'package:online_learing_design/view/sign_up/sign_up.dart';
 import 'package:online_learing_design/view/verify_phone/verify_phone.dart';
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return  const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: VerifyPhone() ,
+      home: Meetup() ,
     );
   }
 }
